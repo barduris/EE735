72bdabee20deed005a0a67db2b2f57f61b837857
Bardur <barduris@gmail.com>
2016-11-29 22:56:52 +0900

    Ready to run all

--- run_p1.sh
+++ run_p1.sh
@@ -5,15 +5,15 @@
 CUDA_VISIBLE_DEVICES=0 th main.lua -task slcls;
 
 # 2. Overfitting.
-#CUDA_VISIBLE_DEVICES=0 th main.lua -task slcls -weightDecay 0;
-#CUDA_VISIBLE_DEVICES=0 th main.lua -task slcls -dropout 0;
-#CUDA_VISIBLE_DEVICES=0 th main.lua -task slcls -augment 0;
-#CUDA_VISIBLE_DEVICES=0 th main.lua -task slcls -net cifarNetLarge;
+CUDA_VISIBLE_DEVICES=0 th main.lua -task slcls -weightDecay 0;
+CUDA_VISIBLE_DEVICES=0 th main.lua -task slcls -dropout 0;
+CUDA_VISIBLE_DEVICES=0 th main.lua -task slcls -augment 0;
+CUDA_VISIBLE_DEVICES=0 th main.lua -task slcls -net cifarNetLarge;
 
 # 3. Loss function.
-#CUDA_VISIBLE_DEVICES=0 th main.lua -task slcls -loss hinge;
-#CUDA_VISIBLE_DEVICES=0 th main.lua -task slcls -loss l2 -learnRate 1e-4,1e-4;
+CUDA_VISIBLE_DEVICES=0 th main.lua -task slcls -loss hinge;
+CUDA_VISIBLE_DEVICES=0 th main.lua -task slcls -loss l2 -learnRate 1e-4,1e-4;
 
 # 4. Convergence speed.
-#CUDA_VISIBLE_DEVICES=0 th main.lua -task slcls -learnRate 1e-3,1e-3;
-#CUDA_VISIBLE_DEVICES=0 th main.lua -task slcls -net cifarNetBatchNorm -learnRate 1e-1,1e-1;
+CUDA_VISIBLE_DEVICES=0 th main.lua -task slcls -learnRate 1e-3,1e-3;
+CUDA_VISIBLE_DEVICES=0 th main.lua -task slcls -net cifarNetBatchNorm -learnRate 1e-1,1e-1;
