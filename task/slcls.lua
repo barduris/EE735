c2d19630c1c8bccde815b36523895ad484f19934
Bardur <barduris@gmail.com>
2016-11-29 21:48:37 +0900

    Removing logsoftmax

--- task/slcls.lua
+++ task/slcls.lua
@@ -364,9 +364,9 @@ function task:defineModel(  )
 		local classifier = nn.Sequential()
 		classifier:add(nn.Reshape(outSize))
 		classifier:add(nn.Linear(outSize, numClass))
-		--if self.opt.loss == 'logSoftMax' then classifier:add(nn.LogSoftMax()) end
-		classifier:add(nn.LogSoftMax())
-		
+		if self.opt.loss == 'logSoftMax' then classifier:add(nn.LogSoftMax()) end
+		--classifier:add(nn.LogSoftMax())
+
 		-- Concatenation
 		model = nn.Sequential()
 		model:add(feature)
